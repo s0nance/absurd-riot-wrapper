0ed35ced69bf37a5b2c53e999fff40acfeeb3b88
Jude <juda777@tutanota.com>
2023-12-04 19:19:04 +0100

    docs: Added useful comments to BaseApi class

--- absurdriotwrapper/apis/BaseApi.py
+++ absurdriotwrapper/apis/BaseApi.py
@@ -10,16 +10,41 @@ class BaseApi:
     """
 
     def __init__(self, api_key):
+        """
+        Initializes a BaseApi instance with the provided Riot API key.
+
+        Args:
+            api_key (str): The Riot API key for authentication.
+        """
         self._api_key = api_key
         self._session = requests.session()
 
     @property
     def api_key(self):
+        """
+        Property method to retrieve Riot API key.
+
+        Returns:
+            str: Riot API key
+        """
         return self._api_key
 
     def request(
         self, url: str, endpoint: str, method: str, region: str, query_params: dict
     ):
+        """
+        Sends an HTTP request to the Riot API.
+
+        Args:
+            url (str): The URL of the Riot API.
+            endpoint (str): The specific endpoint of the API.
+            method (str): The HTTP method to be used.
+            region (str): The region for which the request is made.
+            query_params (dict): Parameters to be included in the API request.
+
+        Returns:
+            requests.Response: The response object from the API request.
+        """
         query_params = {k: v for k, v in query_params.items() if v is not None}
 
         response = None
